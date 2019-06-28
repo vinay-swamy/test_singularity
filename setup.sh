34f8585a26a9b7ad1f6c60b029b81e50a9556e7e
Vinay Swamy <swamyvinny@gmail.com>
2019-06-28 14:42:56 -0400

    spelling

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 git clone https://github.com/bcgsc/transabyss
-mv transabyss/transabyss transabyss/tranabyss.py
+mv transabyss/transabyss transabyss/transabyss.py
 mv transabyss/tranabyss-merge transabyss/transabyss-merge.py
 sudo singularity build transabyss.simg transabyss_container.def
